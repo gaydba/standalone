6494b76daf12c908621e1597fe8433f2b48351dd
Evgeniy Shishkin <eshishki@gmail.com>
2024-08-08 17:39:53 +0400

    disquss идет нахуй

--- juick.js
+++ juick.js
@@ -11,7 +11,6 @@ function setRandomTopic() {
 
 function juickInit(uname) {
   setRandomTopic();
-  initDisquss();
   var message=juickGetHashVar("message");
   daysback=juickGetHashVar("daysback");
   juickTag=juickGetHashVar("tag");
@@ -261,31 +260,6 @@ function juickParseThread(json) {
     li.innerHTML=ihtml;
     replies.appendChild(li);
   }
-  addDisquss(replies, msg[0].mid);
-}
-
-function initDisquss() {
-  (function() { // DON'T EDIT BELOW THIS LINE
-    var d = document, s = d.createElement('script');
-    s.src = 'https://netneladno-ru.disqus.com/embed.js';
-    s.setAttribute('data-timestamp', +new Date());
-    (d.head || d.body).appendChild(s);
-  })();
-}
-
-function addDisquss(parent, mid) {
-  $('#disqus_thread').remove();
-  var disq=document.createElement("div");
-  disq.innerHTML='<div id="disqus_thread"></div>';
-  parent.appendChild(disq);
-  var dconf = function () {
-    this.page.identifier = 'thread_'+mid;
-    this.page.url = 'http://netneladno.ru/#!'+mid;  // Replace PAGE_URL with your page's canonical URL variable
-  };
-  DISQUS.reset({
-    reload: true,
-    config: dconf
-  });
 }
 
 function juickGetHashVar(variable) {
